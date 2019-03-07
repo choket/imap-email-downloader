3d1d3a0ab099982babefba3383da52761e7406c5
Stefan <stefan.stoj@yahoo.com>
2019-03-07 14:24:49 +0100

    Created initial version of email listener that just prints the email number

--- email_listener.py
+++ email_listener.py
@@ -1,4 +1,35 @@
 import server_login
+import imaplib
 
-def email_listen():
-    pass
+
+def email_listen(server_connection, mailbox, search_query):
+    try:
+        server_connection.select(mailbox)
+    except (imaplib.IMAP4.error, imaplib.IMAP4_SSL.error):
+        # TODO implement proper exception handling
+        raise
+
+    response, emails_data = server_connection.search(None, search_query)
+
+    if response != "OK":
+        # TODO implement error handling
+        pass
+
+    emails = emails_data[0].decode().split()
+
+    for i in emails:
+        print(i)
+
+
+def main():
+    server = server_login.server_login(
+        username_or_email="svetlana-sk",
+        password="magdalena",
+        host="mail.net.mk"
+    )
+
+    email_listen(server, "INBOX", "FROM microsoft")
+
+
+if __name__ == '__main__':
+    main()
