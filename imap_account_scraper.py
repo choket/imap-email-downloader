135c4ba61d30e20e7b90724e830e1cd4da0393f6
Stefan <stefan.stoj@yahoo.com>
2019-03-10 23:05:46 +0100

    Fixed line number to take into account the start_offset

--- imap_account_scraper.py
+++ imap_account_scraper.py
@@ -186,6 +186,8 @@ def batch_scrape(file, host=None, port=None, use_ssl=False, login_only=False, fi
             print("skipped")
             next(fh)
         for i, line in enumerate(fh):
+            i += 1 + start_offset
+
             credentials = parse_line(line, delimiter=file_delimiter)
             if credentials is not None:
                 if original_host is None:
