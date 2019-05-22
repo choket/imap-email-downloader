9a251c960708bb88abc8df829f3a317d8b31484b
Stefan <stefan.stoj@yahoo.com>
2019-05-22 16:51:12 +0200

    Moved test data from file directly to the source code

--- tests/test_parse_line_hexadecimal_pytest.py
+++ tests/test_parse_line_hexadecimal_pytest.py
@@ -5,13 +5,17 @@ from imap_account_scraper import parse_line
 
 
 def test_hexadecimal():
-    with open("D:\\password dumps\\extractions\\dot_mk.pog2.txt", encoding="utf-8", errors="ignore") as fh:
-        for line in fh:
-            credentials = parse_line(line)
-            if credentials:
-                password = credentials["password"]
-                assert not re.match(r"^[0-9a-f]{16,}$", password, re.IGNORECASE)
-                print(password)
+    test_lines = (
+        "tony@yahoo.com:1a86be8deadbeef99978",
+        "bob@gmail.com:yoloooo:1684eb764feccda",
+        "21-03-2015:username:afoijl@gmail.com:4ed0b72556bc7b1b2348bfbaf2957676:secret_password",
+    )
+    for line in test_lines:
+        credentials = parse_line(line)
+        if credentials:
+            password = credentials["password"]
+            assert not re.match(r"^[0-9a-f]{16,}$", password, re.IGNORECASE)
+            print(password)
 
 
 if __name__ == "__main__":
